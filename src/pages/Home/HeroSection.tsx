eeb5fc6f3c7169047ab860049f73c20894b25386
ommiputera <omiputerakarunia@gmail.com>
2023-01-05 02:53:16 +0700

    animation update

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -5,33 +5,37 @@ import {Button} from '../../components/Button'
 function HeroSection() {
   return (
     <Wrapper>
-      <LeftContent className="mt-102">
-        <Title className="font-36">
+      <LeftContent className="mt-62">
+        <Title className="font-36 basic-animate">
           Software Engineer and also a Design Enthusiast based in Jakarta.
           Available for freelance and collaborations.
         </Title>
-        <Text size="lg" className="mt-32 font-500">
+        <Text size="lg" className="mt-32 font-500 basic-animate-1">
           Hi 👋, I'm a Software Engineer - Front-end with 2 years of proven
           experience in helping to create and maintain a better code base for
           re-usability and best practices. Experience in developing projects
           from concept to launch. Eager to tackle more complex problems, and
           continues to find ways to maximize user efficiency
         </Text>
-        <Button variant="default" size="lg" className="mt-102">
+        <Button variant="default" size="lg" className="mt-102 basic-animate-2">
           Get started to my open-sources
         </Button>
-        <Button variant="outline" size="lg">
+        <Button variant="outline" size="lg" className="basic-animate-3">
           Learn more about Ommi Putera
         </Button>
       </LeftContent>
       <RightContent className="mt-102">
-        <img className="profileImg" src="/assets/image/profile.jpeg" alt="" />
-        <Text size="lg" className="mt-32 font-500">
+        <img
+          className="profileImg basic-animate-2"
+          src="/assets/image/profile.jpeg"
+          alt=""
+        />
+        <Text size="lg" className="mt-32 font-500 basic-animate-1">
           Jakarta, Indonesia. <div className="line" /> Ommi Putera karunia
           <br />
           omiputrakarunia@gmail.com <div className="line" /> +62 89508182045
         </Text>
-        <Button variant="default" size="lg" className="mt-32">
+        <Button variant="default" size="lg" className="mt-32 basic-animate-3">
           Send me email?
         </Button>
       </RightContent>
